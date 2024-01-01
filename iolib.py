58bb90b6131e388a1df84e627349bd623ab461ea
Mohammad Khazaei <mkhazaei.dev@gmail.com>
2024-01-01 07:06:00 +0330

    update get_numbers: raise an error if the wrong type is selected

--- iolib.py
+++ iolib.py
@@ -1,32 +1,50 @@
 from oslib import clear
 
+
 def get_input(prompt: str) -> str:
-    """_summary_
+    """A function to receive input from the user.
 
     Args:
-        prompt (str): _description_
+        prompt (str): A message displayed to the user for input.
 
     Returns:
-        str: _description_
+        str: Return value from standard input.
     """
     return input(prompt + ": ").capitalize()
 
+
 def get_numbers(number_type: type, count: int) -> list:
+    """A function to get a set of numbers.
+
+    Args:
+        number_type (type): Specifies the numeric data type.
+        count (int): Specifies the count of input numbers.
+
+    Returns:
+        list: Returns the list of input numbers.
+    """
+    acceptable_types = [int, float, complex]
     numbers = []
     index = 0
     error = False
-    while index < count:
-        clear()
-        print("You must enter {} {} numbers".format(count, number_type))
-        if error:
-            print(f"Warning: You did not enter number {index + 1} correctly. Please try again.")
-            error = False
-        number = input(f"{index + 1}. ")
-        try:
-            numbers.append(number_type(number))
-            index += 1
-        except:
-            error = True
-        
+    if number_type in acceptable_types:
+        while index < count:
+            clear()
+            print("You must enter {} \"{}\" numbers".format(count, number_type.__name__))
+            if error:
+                print(f"Warning: You did not enter number {
+                    index + 1} correctly. Please try again.")
+                error = False
+            else:
+                number = input(f"{index + 1}. ")
+            try:
+                numbers.append(number_type(number))
+                index += 1
+            except:
+                error = True
+    else:
+        raise ValueError(f"The type sent is not acceptable. The type must be one of {
+            [item.__name__ for item in acceptable_types]
+            }.")
+
     return numbers
-        
